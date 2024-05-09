d834a013a368c3ecd1338c39ff6cef197a5ba9fb
Guadalupe06Ruiz <guada2806ruiz@gmail.com>
2024-05-09 16:16:45 -0300

    Add files via upload

---
+++ TP-POO/Circulo.cs
@@ -0,0 +1,15 @@
+public class Circulo: Figura
+¨{
+public double Radio ( get; set; )
+
+public Circulo(double radio)
+{
+    Radio=radio;
+}
+public override double CalcularArea()
+{
+    return Math.PI * Math.Pow(Radio. 2);
+}
+
+
+}
---
+++ TP-POO/models/Rectangulo.cs
@@ -0,0 +1,15 @@
+using System.Dynamic;
+
+public class Rectangulo: Figura
+{
+    public double Base.Altura (get ; set ; )
+    
+    public Rectangulo(double Base.Altura)
+    {
+        Base.Altura=base.altura;
+    }
+    public override double CalcularArea()
+    {
+        return Math.
+    }
+}
